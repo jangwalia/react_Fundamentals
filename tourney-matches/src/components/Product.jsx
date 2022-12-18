9006f07418f28791349dbfe0ae7b39e8c792335b
Jang walia <walia@Jangs-MacBook-Air.local>
2022-12-18 14:00:40 -0800

    add Product and ProductList

---
+++ tourney-matches/src/components/Product.jsx
@@ -0,0 +1,16 @@
+function Product() {
+  return (
+    <article className="Product">
+      <h1>
+        Product<span>XXX</span>YYY
+      </h1>
+      {/* To be shown when there is no wins */}
+      <h2 className="zero">Currently There is no discount(</h2>
+      {/* To be shown when there is 1 win */}
+      <h2>Currently offers Discount</h2>
+      {/* To be shown when there is more than one win */}
+      <h2>Customer saved 100 dollars</h2>)
+    </article>
+  );
+}
+export default Product;
---
+++ tourney-matches/src/components/ProductList.jsx
@@ -0,0 +1,10 @@
+import Product from "./Product";
+function ProductList() {
+  return (
+    <section className="ProductList">
+      <h1>Current Products</h1>
+      <Product />
+    </section>
+  );
+}
+export default ProductList;
