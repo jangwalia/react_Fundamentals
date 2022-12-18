9299f3dd31c8e0a155d75086eed88e2b332e3419
Jang walia <walia@Jangs-MacBook-Air.local>
2022-12-18 12:39:49 -0800

    add cra boilerplate

---
+++ tourney-matches/src/_mocks_/orderData.js
@@ -0,0 +1,19 @@
+constOrderSummary = [
+  {
+    orderNumber: 0001,
+    Product: ["3fd0a59a-6949-41cf-860f-bac5cd52e5e2"],
+    originalAmount: 2000,
+    amountPaid: 1500,
+    customerSaved: 500,
+  },
+  {
+    orderNumber: 0002,
+    Product: [
+      "b2b05123-e132-48ed-b07e-4e2366896ffc",
+      "7a200623-473a-4797-95da-49358fe5d526",
+    ],
+    originalAmount: 4000,
+    amountPaid: 4000,
+    customerSaved: 0,
+  },
+];
---
+++ tourney-matches/src/_mocks_/productData.js
@@ -0,0 +1,27 @@
+constProductData = {
+  "3fd0a59a-6949-41cf-860f-bac5cd52e5e2": {
+    productId: "3fd0a59a-6949-41cf-860f-bac5cd52e5e2",
+    productName: "loveChair",
+    type: "furniture",
+    price: 2000,
+    discount: true,
+    availableLocation: ["Newton", "Cloverdale", "Langley"],
+  },
+  "b2b05123-e132-48ed-b07e-4e2366896ffc": {
+    productId: "b2b05123-e132-48ed-b07e-4e2366896ffc",
+    productName: "zoombaTable",
+    type: "furniture",
+    price: 1000,
+    discount: false,
+    availableLocation: ["Newton", "Cloverdale"],
+  },
+
+  "7a200623-473a-4797-95da-49358fe5d526": {
+    productId: "7a200623-473a-4797-95da-49358fe5d526",
+    productName: "smartFridge",
+    type: "appliance",
+    price: 3000,
+    discount: false,
+    availableLocation: ["Delta", "Cloverdale"],
+  },
+};
